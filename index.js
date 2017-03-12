cc74d326d6787f5c531319572629d772fa0d48d9
Adrian Mui <adrian.mui@gmail.com>
2017-03-12 04:41:11 -0500

    added morgan, request logger

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const create = require('./middleware');
 const anon = require('./middleware_anon');
+const morgan = require('morgan');
 
 let app = express();
 
-//middleware stackcs
+/* middleware stacks */
 app.use(create('Adrian Mui middleware'));
+// masking request headers to prevent security attacks
 app.use(anon());
+// request .bash loggers
+app.use(morgan('combined'));
 
 app.get('/hello', (request, response) => {
     response.send(`<h1> Hello ${request.user.name} </h1>`)
