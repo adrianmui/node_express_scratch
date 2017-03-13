f44d64e772c13216407ef74f0ab8674b0599f1b3
Adrian Mui <adrian.mui@gmail.com>
2017-03-13 09:44:00 -0500

    added grunt log, also ignore .vscode

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,13 +14,14 @@ module.exports = function(grunt) {
                 dest: 'public/app.js'
             }
         },
-
-
         uglify: {
             dist: {
                 files: {
                     'public/app.min.js': 'public/app.js'
                 }
+            },
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
             },
             build: {
                 src: './<%= pkg.name %>.js',
@@ -34,5 +35,11 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
 
     // ex: terminal >> grunt 
-    grunt.registerTask('default', ['browserify', 'uglify']);
+    grunt.registerTask('default', ['browserify', 'uglify', 'hasCompleted']);
+
+    // A very basic default task.
+    grunt.registerTask('hasCompleted', 'Log some stuff.', () => {
+        grunt.log.write('All tasks have FINISHED..').ok()
+        grunt.log.write('line 2..').ok();
+    });
 }
